5c5da2e699d173f2786bf4a0a59ff94f7fe16784
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-01 12:34:36 +0200

    output some useful graph

--- bakeChart.Charting/Program.cs
+++ bakeChart.Charting/Program.cs
@@ -1,27 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace bakeChart.Charting
 {
     class Program
     {
+        static Random random = new Random(((int) DateTimeOffset.UtcNow.Ticks));
+        
         static void Main(string[] args)
         {
-            var dic = ReadDataFromFiles(@"c:\git\bakeChart\bakeChart\bin\Debug\outs");
-            var polylines = ToPolylines(dic);
-            var allPolylinesJoined = string.Join("\n", polylines);
-            var chartTemplate = File.ReadAllText("chartTemplate.html");
-            var chart = chartTemplate.Replace("XXXPolylinesXXX", allPolylinesJoined);
-            File.WriteAllText("out.html", chart);
+            var dictionary = ReadDataFromFiles(@"c:\git\bakeChart\bakeChart\bin\Debug\outs");
+            var dictionaryKeys = dictionary.Keys.ToArray();
+            foreach (var key in dictionaryKeys)
+            {
+                //take every 4th entry
+                int c = 0;
+                dictionary[key] = dictionary[key].Where(x => c++ % 4 == 0).ToList();
+            }
+
+            var labels = dictionary
+                .AsEnumerable()
+                .First()
+                .Value
+                .Select(x => "'" + x.DateTime.ToString("O") + "'")
+                .Aggregate((a,b) => a + "," + b);
+
+            var allDataSetsJoined = dictionary.Select(x => DatasetForCompetitor(x.Key, x.Value)).Aggregate((a, b) => a + "\n" + b);
+            var allText = File.ReadAllText("chartTemplate.html");
+            var replace = allText
+                .Replace("XXXLabelsXXX", labels)
+                .Replace("XXXDataSetsXXX", allDataSetsJoined);
+            File.WriteAllText("out.html", replace);
+        }
+
+        private static string DatasetForCompetitor(string name, List<Point> points)
+        {
+            var data = points
+                .Select(p => p.Value.ToString())
+                .Aggregate((a, b) => a + "," + b);
+
+            var colorNames = new [] {"red","orange","yellow","green","blue","purple","grey"};
+            var colorName = colorNames[random.Next(colorNames.Length)];
+            
+            var sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine("label: '" + name + "',");
+            sb.AppendLine("backgroundColor: window.chartColors." + colorName + ",");
+            sb.AppendLine("borderColor: window.chartColors." + colorName + ",");
+            sb.AppendLine("data: [");
+            sb.AppendLine(data);
+            sb.AppendLine("    ],");
+            sb.AppendLine("fill: false,");
+            sb.AppendLine("},");
+
+            return sb.ToString();
         }
 
         private static Dictionary<string, List<Point>> ReadDataFromFiles(string pathToDirectoryWithFiles)
         {
-            var dic = new Dictionary<string, List<Point>>();
+            var dictionary = new Dictionary<string, List<Point>>();
             var files = Directory.GetFiles(pathToDirectoryWithFiles);
             foreach (var file in files)
             {
@@ -33,45 +74,16 @@ namespace bakeChart.Charting
                     var splitted = line.Split('\t');
                     var competitorName = splitted[0];
                     var value = int.Parse(splitted[1]);
-                    if (!dic.ContainsKey(competitorName))
+                    if (!dictionary.ContainsKey(competitorName))
                     {
-                        dic.Add(competitorName, new List<Point>());
+                        dictionary.Add(competitorName, new List<Point>());
                     }
 
-                    dic[competitorName].Add(new Point {DateTime = dateTimeFromFileName, Value = value});
+                    dictionary[competitorName].Add(new Point {DateTime = dateTimeFromFileName, Value = value});
                 }
             }
 
-            return dic;
-        }
-
-        private static IEnumerable<string> ToPolylines(Dictionary<string, List<Point>> dic)
-        {
-            var polylineWithPlaceholders = "<polyline fill=\"none\" stroke=\"#XXXXColorXXX\" stroke-width=\"1\" points=\"XXXPointsXXX\"/>";
-            var random = new Random(DateTimeOffset.UtcNow.Millisecond);
-
-            foreach (var keyValuePair in dic)
-            {
-                var points = keyValuePair.Value;
-                
-                Color randomColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
-                string randomColorAsHex = randomColor.R.ToString("X2") + randomColor.G.ToString("X2") + randomColor.B.ToString("X2");
-                
-                var polylinePoints = points
-                    .OrderBy(point => point.DateTime)
-                    .Select(point =>
-                    {
-                        var x = ((point.DateTime.UtcTicks - 636659278810000000) / 100000000).ToString();
-                        var y = point.Value;
-                        return x + "," + y;
-                    })
-                    .Aggregate((a, b) => a + " " + b);
-
-                var polylineFilled = polylineWithPlaceholders
-                    .Replace("XXXXColorXXX", randomColorAsHex)
-                    .Replace("XXXPointsXXX", polylinePoints);
-                yield return polylineFilled;
-            }
+            return dictionary;
         }
     }
 
