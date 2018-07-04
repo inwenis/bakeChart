b5ed5b5e01041a63c56b4a7a17b3de13f6c5e1f1
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-04 11:40:26 +0200

    add simple tool to migrate files from old structure to new

---
+++ migrateOldOutputToNew/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace migrateOldOutputToNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outsPath = args[0];
+            var files = Directory.GetFiles(outsPath);
+            foreach (var file in files)
+            {
+                var dateTimeFromFileName = DateTimeOffset.ParseExact(Path.GetFileNameWithoutExtension(file), "yyyy-MM-ddTHH-mm-ss", new DateTimeFormatInfo());
+                var outputDirecotry = Path.Combine(
+                    dateTimeFromFileName.Year.ToString("0000"),
+                    dateTimeFromFileName.Month.ToString("00"),
+                    dateTimeFromFileName.Day.ToString("00"),
+                    dateTimeFromFileName.Hour.ToString("00"));
+                Directory.CreateDirectory(outputDirecotry);
+                File.Copy(file, Path.Combine(outputDirecotry, Path.GetFileName(file)));
+                File.Delete(file);
+            }
+        }
+    }
+}
