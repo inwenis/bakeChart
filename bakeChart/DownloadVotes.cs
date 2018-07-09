b84c15e13f8967652d668e6acfc96d52ac5543ec
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-09 19:06:24 +0200

    add area name to results and console project for testing

--- bakeChart/DownloadVotes.cs
+++ bakeChart/DownloadVotes.cs
@@ -6,7 +6,7 @@ using HtmlAgilityPack;
 
 namespace bakeChart
 {
-    class DownloadVotes
+    public class DownloadVotes
     {
         private static string _url;
 
@@ -31,20 +31,24 @@ namespace bakeChart
             return result;
         }
 
-        private static string Parse(string result)
+        public static string Parse(string result)
         {
-            var xPath = "//*[@id=\"gtsms_sms-wyniki\"]/ul/li";
+            var xPathToAreaName = "//*[@id=\"gtsms_sms-wyniki\"]/h2";
+            var xPathToCompetitorListItem = "//*[@id=\"gtsms_sms-wyniki\"]/ul/li";
             StringReader reader = new StringReader(result);
             var doc = new HtmlDocument();
             doc.Load(reader);
-            var nodes = doc.DocumentNode.SelectNodes(xPath);
+
+            var areaName = doc.DocumentNode.SelectNodes(xPathToAreaName)[0].InnerText;
+
+            var nodes = doc.DocumentNode.SelectNodes(xPathToCompetitorListItem);
             var stringBuilder = new StringBuilder();
             foreach (var node in nodes)
             {
                 var name = node.SelectNodes("./span[2]/span/span[1]/strong")[0].InnerText;
                 var count = node.SelectNodes("./span[2]/span/span[2]/text()")[0].InnerText;
                 count = count.Trim();
-                stringBuilder.AppendLine(name + "\t" + count);
+                stringBuilder.AppendLine(name + "\t" + areaName + "\t" + count);
             }
 
             return stringBuilder.ToString();
---
+++ testBakeChart/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bakeChart;
+
+namespace testBakeChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contant = File.ReadAllText(@"c:\stuff\Hermes.html");
+            var parsed = DownloadVotes.Parse(contant);
+            Console.WriteLine(parsed);
+            Console.ReadLine();
+        }
+    }
+}
